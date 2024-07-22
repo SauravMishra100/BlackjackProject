034b09f43929acf06502587ebf76d54180370e37
SauravMishra100 <59577143+SauravMishra100@users.noreply.github.com>
2024-07-21 20:38:22 -0400

    Add files via upload

---
+++ Blackjack/04_Milestone/Blackjack_GUI/src/Blackjack.java
@@ -0,0 +1,37 @@
+/**
+ * This is the main program for the blackjack game
+ */
+public class Blackjack {
+
+   /**
+    * This method should:
+    * <ul>
+    *    <li>Ask the user how many people want to play (up to 3, not including the dealer).</li>
+    *    <li>Create an array of players.</li>
+    *    <li>Create a Blackjack window.</li>
+    *    <ul>
+    *       <li>Play rounds until the players want to quit the game.</li>
+    *    </ul>
+    *    <li>Close the window</li>
+    * </ul>
+    * @param args Arguments
+    */
+   public static void main(String[] args) {
+      // complete this method
+   }
+
+   /**
+    * This method executes an single round of play (for all players).  It should:
+    * <ul>
+    *    <li>Create and shuffle a deck of cards.</li>
+    *    <li>Start the round (deal cards) for each player, then the dealer.</li>
+    *    <li>Allow each player to play, then the dealer.</li>
+    *    <li>Finish the round (announce results) for each player.</li>
+    * </ul>
+    * @param players Array of players
+    * @param window Blackjack window
+    */
+   public static void playRound(Player[] players, BlackjackWindow window) {
+      // complete this method
+   }
+}
---
+++ Blackjack/04_Milestone/Blackjack_GUI/src/Card.java
@@ -0,0 +1,86 @@
+/**
+ * This class represents one playing card.
+ */
+public class Card {
+   // Card suits (provided for your convenience - use is optional)
+   public static final int SPADES   = 0;
+   public static final int HEARTS   = 1;
+   public static final int CLUBS    = 2;
+   public static final int DIAMONDS = 3;
+
+   // Card faces (provided for your convenience - use is optional)
+   public static final int ACE   = 1;
+   public static final int TWO   = 2;
+   public static final int THREE = 3;
+   public static final int FOUR  = 4;
+   public static final int FIVE  = 5;
+   public static final int SIX   = 6;
+   public static final int SEVEN = 7;
+   public static final int EIGHT = 8;
+   public static final int NINE  = 9;
+   public static final int TEN   = 10;
+   public static final int JACK  = 11;
+   public static final int QUEEN = 12;
+   public static final int KING  = 13;
+
+
+   // define fields here
+
+   /**
+    * This constructor builds a card with the given suit and face, turned face down.
+    * @param cardSuit Card suit
+    * @param cardFace Card face
+    */
+   public Card(int cardSuit, int cardFace) {
+      // complete this method
+   }
+
+   /**
+    * This method retrieves the suit (spades, hearts, etc.) of this card.
+    * @return Card suit
+    */
+   public int getSuit() {
+      return 0; // replace this line with your code
+   }
+
+   // This method retrieves the face (ace through king) of this card.
+
+   /**
+    * This method retrieves the face (ace through king) of this card.
+    * @return Face of card
+    */
+   public int getFace() {
+      return 0; // replace this line with your code
+   }
+
+   /**
+    * This method retrieves the numerical value of this card
+    * (usually same as card face, except 1 for ace and 10 for jack/queen/king)
+    * @return Value of card
+    */
+   public int getValue() {
+      return 0; // replace this line with your code
+   }
+
+   /**
+    * This method determines whether the front of the card should be visible.
+    * @return True if card is face up
+    */
+   public boolean isFaceUp() {
+      return false; // replace this line with your code
+   }
+
+   /**
+    * This method records that the front of the card should be visible.
+    */
+   public void turnFaceUp() {
+      // complete this method
+   }
+
+   /**
+    * This method records that only the back of the card should be visible.
+    */
+   public void turnFaceDown() {
+      // complete this method
+   }
+}
